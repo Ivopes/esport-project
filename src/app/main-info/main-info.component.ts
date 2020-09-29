5bd85825baa33d9eafad050cfb3d180485aee645
Ivo Pesak <hapesak@seznam.cz>
2020-09-29 15:55:45 +0200

    stuff

--- src/app/main-info/main-info.component.ts
+++ src/app/main-info/main-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../shared/user';
 
 @Component({
   selector: 'app-main-info',
@@ -7,6 +8,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./main-info.component.scss']
 })
 export class MainInfoComponent implements OnInit {
+
+  lahev: User = {
+    Title: 'Profi lahev',
+    SubTitle: 'Uhasí touhu po výhře',
+    PhotoUrl: 'assets/merch/lahev_merch.png',
+    Content: 'Profi Esport lahev pro pravé hráče. Nezapomínej na pitný režim!!!'
+  };
+  mikina: User = {
+    Title: 'Profi mikina',
+    SubTitle: 'Zahřeje tě',
+    PhotoUrl: 'assets/merch/cervena_mikina_merch.png',
+    Content: 'Stylová červená mikina s tvým oblíbeným týmem - naším!!!'
+  };
+  sede: User = {
+    Title: 'Profi šedé tričko',
+    SubTitle: 'Obleč se stylově',
+    PhotoUrl: 'assets/merch/sede_triko_merch.jpg',
+    Content: 'Stylové šedé tričko s tvým oblíbeným týmem - naším!!!'
+  };
+  modre: User = {
+    Title: 'Profi modré tričko',
+    SubTitle: 'Obleč se stylově',
+    PhotoUrl: 'assets/merch/svetle_modre_triko_merch.jpg',
+    Content: 'Stylová modré mikina s tvým oblíbeným týmem - naším!!!'
+  };
+  cepice: User = {
+    Title: 'Profi kšiltovka',
+    SubTitle: 'Kryje před headshotem',
+    PhotoUrl: 'assets/merch/ksiltovka_merch.png',
+    Content: 'Stylová kšiltovka s tvým oblíbeným týmem - naším!!! S touhle ti headshot jenom tak nedají...'
+  };
+  rouska: User = {
+    Title: 'Profi rouška',
+    SubTitle: 'Nebuď toxic',
+    PhotoUrl: 'assets/merch/rouska_merch.png',
+    Content: 'Nemůže tě otrávit toxic komunita, když máš roušku přímo od nás!!!'
+  };
 
   constructor(
     private router: Router
--- src/app/main-team/main-team.component.ts
+++ src/app/main-team/main-team.component.ts
@@ -23,34 +23,34 @@ import { User } from '../shared/user';
 export class MainTeamComponent implements OnInit {
 
   ivo: User = {
-    Title: 'Ivo',
-    SubTitle: 'Rimmer',
-    PhotoUrl: 'assets/userPic/ivo.png',
-    Content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proident'
+    Title: 'Ivo Pešák',
+    SubTitle: 'Kostěj',
+    PhotoUrl: 'assets/userPic/ivo.jpg',
+    Content: 'Ročník 1998. Jsem bývalý hráč Doty, na které mám přes 1500 hodin. Poté přesun na CS:GO ve kterém mám něco málo přes 1000 hodin. Jsem výborný hráč, který umí hrát nejenom na aim, ale taky na brain. Nebojím se sem tam i něco callnout. Občas si zanadávám když borec proti mě náhodou trefí něco co by v životě jinak netrefil ani kdyby chtěl. Rád hraju se svým spoluhráčem Huldou !S!, je mi jasné co udělá ještě dřív než to ví on sám. Moje nejoblíbenější zbraň je AK-47, nejoblíbenější mapa je inferno. Můj největší nepřítel – PING. '
   };
   hulda: User = {
-    Title: 'Hulda',
-    SubTitle: 'Rimmer',
-    PhotoUrl: 'assets/userPic/hulda.png',
-    Content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proident'
+    Title: 'Daniel Hulda',
+    SubTitle: 'Hulda !S!',
+    PhotoUrl: 'assets/userPic/hulda.jpg',
+    Content: 'Narodil jsem se 15.10 1998. Od mala jsem již “buřil“ co se dalo, myslím tím samozřejmě hry. Nejoblíbenější herní tituly pro mě byly tekken 3, destruction derby a jednoznačně crash bandicoot. U každé hry je pro mě důležité makro. Sleduju všechny možné profíky, abych věděl u dané hry všechny taktiky, protože jak se říká: “Kdo je připraven, není překvapen.“ Jako hráč jsem dosti agresivní a často rageuju. Proto si rád zahraju i nějakou tu oddechovku jako je třeba “Snail Bob“. Všem doporučuji. V csku jsem entry frager a nejradši se spoléhám na taktiku “spray \'n pray“. O našem týmu si myslím, že se krásně doplňujeme a v gamingu patříme určitě ke špici.'
   };
   denis: User = {
     Title: 'Denis Lokaj',
     SubTitle: 'DeniGOD',
-    PhotoUrl: 'assets/userPic/denis.png',
+    PhotoUrl: 'assets/userPic/denis.jpg',
     Content: 'Narozen 24.7.1998 ve Frýdku-Místku. Jako malého mě počítačové hry fascinovaly a rád jsem hraním trávil svůj volný čas, v této době jsem ale ještě netušil, že právě tyhle hry ve mně později vzbudí zvídavost v informačních technologiích, ať už se jedná o hardware nebo software. Nyní jsem už dospělý a volný čas trávím především jinak, hry nejsou moje každodenní náplň a z velké části mě omrzely, ale zájem o technologii „pod pokličkou“ vzkvétá. Ve svém volném čase rád posedím s přáteli, rekreačně sportuju, lyže, kolo, plavání, cvičení. Občas si přečtu knihu na téma, které mě momentálně zajímá, poslouchám hudbu a když je chuť, tak i sem tam něco poživatelného uvařím.'
   };
   jirka: User = {
-    Title: 'Jirka',
-    SubTitle: 'Rimmer',
+    Title: 'Jiří Mann',
+    SubTitle: 'Levi',
     PhotoUrl: 'assets/userPic/Jirka.jpg',
-    Content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proidentem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proidentem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proidentem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proident'
+    Content: 'V Českém Těšíně se dne 24.5. 1999 v nemocnici narodil neskutečný beast, který má již od svého raného dětství pouze jeden cíl - dominovat zápasy v CSku. Poté co jsem dosáhl ranku Global Elite v CSku je mým stávajícím úkolem získat level 10 na faceitu. Pomalu ale jistě se tomuto životnímu "achievementu" blížím.  Jako správný gamer piji hromady energeťáků každý den, nejradši mam RockStar s přichutí Guava. Mimo jiné jsem našel v posledních letech zálibu v informačních technologiích, i když má láska k CSku stále přesiluje. Jsem jednoznačně pokladem v našem týmu. 🙂'
   };
   kominek: User = {
-    Title: 'Pan Kominek',
-    SubTitle: 'Rimmer',
-    PhotoUrl: 'assets/userPic/kominek.png',
-    Content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat. Proin in tellus sit amet nibh dignissim sagittis. Excepteur sint occaecat cupidatat non proident'
+    Title: 'Daniel Kominek',
+    SubTitle: 'Danny',
+    PhotoUrl: 'assets/userPic/kominek.jpg',
+    Content: 'Nemocnice ve Frýdku-Místku měla jeden velmi zajímavý den ve své historii - 18.11. 1998 jsem přišel na svět. Již od mala jsem měl veliký problém se odtrhnout od PC her a moje největší srdcovka od dětství je titul jménem "Return to castle Wolfenstein", na druhém místě po něm následoval CS:1.6, který jsem byl nucen z důvodu špatného internetu hrát pouze s botama. V posledním roce svůj volný čas trávím hlavně s lidmi, při sportu nebo v práci a na hry již moc času nemám. Informatika mě vždy zajímala a bavila a to se už jen tak nezmění.'
   };
 
   constructor(
